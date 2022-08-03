fd23655ab4313c83e051c434ef6e2acb43a6db25
MO7AMMED-ISMAIL <mo7medismail200@gmail.com>
2022-08-03 20:51:35 +0200

    new repo

---
+++ courses/php/session 2/use_require_include.php
@@ -0,0 +1,27 @@
+<?php
+
+include_once 'include.php';
+
+echo '<br/>';
+
+include 'include.php';
+
+include_once 'include.php';
+
+echo '<br/>-----------------------<br/>';
+echo '<br/>-----------------------<br/>';
+echo '<br/>-----------------------<br/>';
+
+echo '<br/>';
+
+echo $include_var;
+echo '<br/>-----------------------<br/>';
+echo '<br/>-----------------------<br/>';
+
+echo '<br/>';
+
+require 'require.php';
+
+echo '<br/>';
+
+echo $require_var;
