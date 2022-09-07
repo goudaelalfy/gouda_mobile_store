90c4b2a2266f2f6c93c49ef7d7467bfca77da0f8
Gouda Elalfy <goudaelalfy@gmail.com>
2022-09-07 22:56:03 +0300

    OOP 2

--- interfaces.php
+++ interfaces.php
@@ -4,6 +4,10 @@ interface Animal {
   public function makeSound();
 }
 
+interface Fruits {
+  public function color();
+}
+
 // Class definitions
 class Cat implements Animal {
   public function makeSound() {
@@ -11,8 +15,21 @@ class Cat implements Animal {
   }
 }
 
-class Dog implements Animal {
+
+class Dog  implements Animal{
   public function makeSound() {
+    echo " Bark ";
+  }
+  
+  public function eat() {
+    echo " Bark ";
+  }
+  
+  public function drink() {
+    echo " Bark ";
+  }
+
+  public function sss() {
     echo " Bark ";
   }
 }
@@ -23,13 +40,130 @@ class Mouse implements Animal {
   }
 }
 
+
+abstract class Fish implements Animal {
+    
+  abstract public function eat();
+  
+  public function drink() {
+      echo 'water';
+  }
+}
+
+class Salmaon extends Fish {
+    
+    public function makeSound() {
+        echo " Sal Sound ";
+    }
+  
+    public function eat() {
+        echo 'young fish';
+    }
+    
+    public function eat($a) {
+        echo 'young fish';
+    }
+    
+    public function eat($a, $b) {
+        echo 'young fish';
+    }
+  
+    
+  
+}
+
+
+
+trait Monkey {
+  public function makeSound() {
+    echo " Squeak ";
+  }
+}
+
+trait Lion {
+  public function makeDuty() {
+    echo " Duty ";
+  }
+}
+
+class x {
+  use Lion, Monkey;
+  
+}
+
+class Macarel extends Salmaon {
+    
+    public function makeSound() {
+        echo " Macarel Sound ";
+    }
+    
+}
+
+class Sss extends Salmaon  {
+    
+    public function makeSound() {
+        echo " Macarel Sound ";
+    }
+    
+     public function color() {
+         
+     }
+    
+}
+
+
 // Create a list of animals
 $cat = new Cat();
 $dog = new Dog();
 $mouse = new Mouse();
-$animals = array($cat, $dog, $mouse);
+
+//$fish = new Fish();
+$salmaon = new Salmaon();
+
+$macarel = new Macarel();
+
+
+$animals = array($cat, $dog, $mouse, $salmaon, $macarel);
 
 // Tell the animals to make a sound
 foreach($animals as $animal) {
-  $animal->makeSound();
+  echo '<br/>';
+  //$animal->makeSound();
 }
+
+
+
+class AccessModifier {
+    
+    private $x;
+    protected $y;
+    
+    private function privateFunction() {
+        echo 'private';
+    }
+    
+    protected function protectedFunction() {
+        echo 'protected';
+        
+    }
+    
+    public function publicFunction() {
+        echo 'public';
+        
+    }
+    
+}
+
+class ExtendAccessModifier extends AccessModifier {
+    
+    function x() {
+        $this->protectedFunction();
+    }
+    
+}
+
+$access_modifier = new AccessModifier();
+//$access_modifier->protectedFunction();
+
+$e_access_modifier = new ExtendAccessModifier();
+$e_access_modifier->x();
